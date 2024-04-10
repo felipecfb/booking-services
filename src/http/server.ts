9d4b5a40caf2222980a2a5fc42d5b6336c8bc33f
Felipe Bastos <felipe.bastos.dev@gmail.com>
2024-04-10 11:32:05 -0300

    feat: adding and configuring eslint

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,15 +1,16 @@
-import fastify from "fastify"
+import fastify from 'fastify'
 
 const app = fastify()
 
-app.get('/', () =>  {
+app.get('/', () => {
   return 'Hello World!'
 })
 
-app.listen({
-  port: 3333,
-  host: '0.0.0.0'
-}).then(() => {
-  console.log('🔥 HTTP server running!');
-  
-})
+app
+  .listen({
+    port: 3333,
+    host: '0.0.0.0',
+  })
+  .then(() => {
+    console.log('🔥 HTTP server running!')
+  })
