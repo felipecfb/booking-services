70a08df336e909889ca18a782bb0d606bcf6424a
Felipe Bastos <felipe.bastos.dev@gmail.com>
2024-04-10 10:57:53 -0300

    initial commit

---
+++ src/http/server.ts
@@ -0,0 +1,15 @@
+import fastify from "fastify"
+
+const app = fastify()
+
+app.get('/', () =>  {
+  return 'Hello World!'
+})
+
+app.listen({
+  port: 3333,
+  host: '0.0.0.0'
+}).then(() => {
+  console.log('🔥 HTTP server running!');
+  
+})
