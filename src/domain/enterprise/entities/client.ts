f94fb5f28d198a22cddd679c0b0b441f66822db8
Felipe Bastos <felipe.bastos.dev@gmail.com>
2024-04-22 11:32:14 -0300

    chore: update core entities folder

--- src/domain/enterprise/entities/client.ts
+++ src/domain/enterprise/entities/client.ts
@@ -1,5 +1,5 @@
-import { Entity } from '../../../core/entity'
-import { UniqueEntityID } from '../../../core/unique-entity-id'
+import { Entity } from '../../../core/entities/entity'
+import { UniqueEntityID } from '../../../core/entities/unique-entity-id'
 
 export interface ClientProps {
   name: string
