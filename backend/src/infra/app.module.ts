f33975bc5948809159b6ef7ac32e25011ec21002
Felipe Bastos <felipe.bastos.dev@gmail.com>
2024-06-10 19:07:16 -0300

    feat: creating auth module and service

--- backend/src/infra/app.module.ts
+++ backend/src/infra/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 import { envSchema } from './env'
 import { EnvModule } from './env/env.module'
+import { AuthModule } from './auth/auth.module'
 
 @Module({
   imports: [
@@ -9,6 +10,7 @@ import { EnvModule } from './env/env.module'
       validate: (env) => envSchema.parse(env),
       isGlobal: true,
     }),
+    AuthModule,
     EnvModule,
   ],
 })
