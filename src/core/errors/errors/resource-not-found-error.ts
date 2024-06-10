06ba32a831bfdb1a5e5102344831a7ddb82610aa
Felipe Bastos <felipe.bastos.dev@gmail.com>
2024-06-10 18:18:21 -0300

    feat: adding nest to project

--- src/core/errors/errors/resource-not-found-error.ts
+++
@@ -1,7 +0,0 @@
-import { UseCaseError } from '@/core/errors/use-case-error'
-
-export class ResourceNotFoundError extends Error implements UseCaseError {
-  constructor(message?: string) {
-    message ? super(message) : super('Resource not found')
-  }
-}
--- src/domain/application/repositories/establishments-repository.ts
+++
@@ -1,6 +0,0 @@
-import { Establishment } from '@/domain/enterprise/entities/establishment'
-
-export interface EstablishmentsRepository {
-  findEstablishmentById(establishmentId: string): Promise<Establishment | null>
-  create(establishment: Establishment): Promise<Establishment>
-}
