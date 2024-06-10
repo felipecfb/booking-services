b9edad97061034d45163a598a8d8b1905c1a9e2a
Felipe Bastos <felipe.bastos.dev@gmail.com>
2024-06-10 19:01:47 -0300

    feat: creating env file modules and services

--- backend/src/env.ts
+++
@@ -1,9 +0,0 @@
-import 'dotenv/config'
-import { z } from 'zod'
-
-const envSchema = z.object({
-  PORT: z.coerce.number().default(3333),
-  DATABASE_URL: z.string().url(),
-})
-
-export const env = envSchema.parse(process.env)
