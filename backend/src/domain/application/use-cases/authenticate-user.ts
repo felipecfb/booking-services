867faa3daaa31f97f9d84740147d4cd0d4e98971
Felipe Bastos <felipe.bastos.dev@gmail.com>
2024-06-11 11:29:31 -0300

    feat: creating flux for authenticate user

--- backend/src/domain/application/use-cases/authenticate-user.ts
+++ backend/src/domain/application/use-cases/authenticate-user.ts
@@ -3,6 +3,7 @@ import { WrongCredentialsError } from './errors/wrong-credentials-error'
 import { HashComparer } from '../cryptograpy/hash-comparer'
 import { Encrypter } from '../cryptograpy/encrypter'
 import { UsersRepository } from '../repositories/users-repository'
+import { Injectable } from '@nestjs/common'
 
 interface AuthenticateUserUseCaseRequest {
   email: string
@@ -16,6 +17,7 @@ type AuthenticateUserUseCaseResponse = Either<
   }
 >
 
+@Injectable()
 export class AuthenticateUserUseCase {
   constructor(
     private usersRepository: UsersRepository,
@@ -44,6 +46,7 @@ export class AuthenticateUserUseCase {
 
     const accessToken = await this.encrypter.encrypt({
       sub: user.id.toString(),
+      establishmentId: user.establishmentId,
     })
 
     return right({
--- backend/src/infra/http/http.module.ts
+++ backend/src/infra/http/http.module.ts
@@ -7,10 +7,20 @@ import { CreateEstablishmentUseCase } from '@/domain/application/use-cases/creat
 import { RegisterUserController } from './controllers/register-user.controller'
 import { RegisterUserUseCase } from '@/domain/application/use-cases/register-user'
 import { CryptographyModule } from '../cryptography/cryptography.module'
+import { AuthenticateController } from './controllers/authenticate.controller'
+import { AuthenticateUserUseCase } from '@/domain/application/use-cases/authenticate-user'
 
 @Module({
   imports: [DatabaseModule, CryptographyModule],
-  controllers: [CreateEstablishmentController, RegisterUserController],
-  providers: [CreateEstablishmentUseCase, RegisterUserUseCase],
+  controllers: [
+    CreateEstablishmentController,
+    RegisterUserController,
+    AuthenticateController,
+  ],
+  providers: [
+    CreateEstablishmentUseCase,
+    RegisterUserUseCase,
+    AuthenticateUserUseCase,
+  ],
 })
 export class HttpModule {}
