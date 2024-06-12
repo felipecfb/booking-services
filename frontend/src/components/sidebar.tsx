a5fa25e34146d1c759878ff686f59107244b6791
Felipe Bastos <felipe.bastos.dev@gmail.com>
2024-06-12 09:49:59 -0300

    style: update html sidebar

--- frontend/src/components/sidebar.tsx
+++ frontend/src/components/sidebar.tsx
@@ -8,20 +8,18 @@ import { Separator } from './ui/separator'
 export function Sidebar() {
   return (
     <div className="flex flex-col justify-between min-h-screen w-1/5 bg-zinc-800 py-4">
-      <div>
-        <div className="w-full flex flex-col items-center gap-4 py-4">
-          <Link to="/">
-            <PlaneTakeoff size={64} className="text-zinc-50" />
-          </Link>
-          <Separator className="w-full h-0.5" />
-        </div>
+      <section className="w-full flex flex-col items-center gap-4 py-4">
+        <Link to="/">
+          <PlaneTakeoff size={64} className="text-zinc-50" />
+        </Link>
+        <Separator className="w-full h-0.5" />
+      </section>
 
-        <div className="flex flex-col items-center gap-2">
-          <Avatar className="w-14 h-14">
-            <AvatarImage src="https://gravatar.com/avatar/fd876f8cd6a58277fc664d47ea10ad19?s=200&d=robohash&r=g" />
-          </Avatar>
-          <h2 className="text-zinc-50 text-lg font-bold">John Doe</h2>
-        </div>
+      <section className="flex flex-col items-center gap-2">
+        <Avatar className="w-14 h-14">
+          <AvatarImage src="https://gravatar.com/avatar/fd876f8cd6a58277fc664d47ea10ad19?s=200&d=robohash&r=g" />
+        </Avatar>
+        <h2 className="text-zinc-50 text-lg font-bold">John Doe</h2>
 
         <Nav.Root>
           <Nav.Link to="/">
@@ -37,12 +35,14 @@ export function Sidebar() {
             <span>Profile</span>
           </Nav.Link>
         </Nav.Root>
-      </div>
+      </section>
 
-      <Button className="bg-transparent text-zinc-50 space-x-2 hover:bg-transparent justify-start w-max font-bold">
-        <LogOut size={20} />
-        <span>Sign out</span>
-      </Button>
+      <section className="flex-1 flex items-end justify-start">
+        <Button className="bg-transparent text-zinc-50 space-x-2 hover:bg-transparent justify-start w-max font-bold">
+          <LogOut size={20} />
+          <span>Sign out</span>
+        </Button>
+      </section>
     </div>
   )
 }
